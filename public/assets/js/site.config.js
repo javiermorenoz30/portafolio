541b8fa8fa6103e19e4645cc810e7a12dcfb4f0c
javiermorenoz30 <84947673+javiermorenoz30@users.noreply.github.com>
2026-08-14 17:38:30 -0400

    Astro migration: remove legacy JavaScript

--- public/assets/js/site.config.js
+++
@@ -1,7 +0,0 @@
-window.SITE_CONFIG = {
-  email: "hello@javiermoreno.studio",
-  instagram: "https://instagram.com/",
-  behance: "https://behance.net/",
-  linkedin: "https://linkedin.com/",
-  location: "Caracas / Worldwide"
-};
